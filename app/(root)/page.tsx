1e83e1da3e889c3848ba72c4144a395ec40cacb5
RPahade <rohpahade2@gmail.com>
2024-07-14 18:59:21 +0530

    Implement and commit the whole layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName:'Adrian', lastName : "jsm", email: 'contact@jsm.pro'}
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+           type="greeting"
+           title='Welcome'
+           user = {loggedIn?.firstName || 'Guest'}
+           subtext = 'Access and manage your account transactions efficiently'
+          />
+          <TotalBalanceBox
+          accounts = {[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.23}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+      user = {loggedIn}
+      transactions = {[]}
+      banks = {[{currentBalance:120.23}, {currentBalance:500.23}]}
+      />
+    </section>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const ibmPlexSerif = IBM_Plex_Serif({
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platform for everyone",
+  icons:'/icons/logo.svg'
 };
 
 export default function RootLayout({
